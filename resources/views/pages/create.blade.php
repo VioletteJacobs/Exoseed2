05c9940d279502657430b73c7fce0ac08db51550
Violette Jacobs <72454626+VioletteJacobs@users.noreply.github.com>
2021-02-25 09:35:40 +0100

    old create

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -17,31 +17,31 @@
         @csrf
         <div class="form-group">
             <label >Compagny's Name</label>
-            <input type="Name" name="Compagny" class="form-control" >
+            <input type="Name" name="Compagny" value="{{old("Compagny")}}" class="form-control" >
         </div>
         <div class="form-group">
             <label >Compagny's Adress</label>
-            <input type="Adress" name="Adresse" class="form-control" >
+            <input type="Adress" name="Adresse" value="{{old("Adresse")}}" class="form-control" >
         </div>
         <div class="form-group">
             <label >Compagny's Phone</label>
-            <input type="Phone" name="phone" class="form-control" >
+            <input type="Phone" name="phone" value="{{old("phone")}}"  class="form-control" >
         </div>
         <div class="form-group">
             <label >Compagny's Email</label>
-            <input type="Email" name="email" class="form-control" >
+            <input type="Email" name="email" value="{{old("email")}}" class="form-control" >
         </div>
         <div class="form-group">
             <label >contact's name</label>
-            <input type="name" name="name" class="form-control" >
+            <input type="name" name="name" value="{{old('name')}}" class="form-control" >
         </div>
         <div class="form-group">
             <label >contact's surname</label>
-            <input type="surname" name="surname" class="form-control" >
+            <input type="surname" name="surname" value="{{old('surname')}}" class="form-control" >
         </div>
         <div class="form-group">
             <label >SRC</label>
-            <input type="src" name="src" class="form-control" >
+            <input type="src" name="src" value="{{old('src')}}" class="form-control" >
         </div>
         <button type="submit" class="btn btn-primary">Créer</button>
     </form>
